1bd4e6d3cbfdfd39abc9eeba492b582114e63ce1
konsultan.it <konsulkan.it@gmail.com>
2023-09-20 23:13:58 +0700

    pertama

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Siprestof</title>
+    <link rel="stylesheet" href="./soruce/style/login.css" />
+    <link rel="icon" type="image/png" href="./soruce/asset/Icon_Web.png" />
+  </head>
+
+  <body>
+    <form action="" method="post">
+      <img
+        src="./soruce/asset/Logo_Utama.png"
+        class="Logo_Utama"
+        alt="logo utama"
+      />
+      <div class="input-with-icon">
+        <img src="./soruce/asset/user-icon.png" alt="User Icon" class="icon" />
+        <input type="text" placeholder="Username" />
+      </div>
+
+      <div class="input-with-icon">
+        <img
+          src="./soruce/asset/password-icon.png"
+          alt="Password Icon"
+          class="icon"
+        />
+        <input type="password" placeholder="Password" />
+      </div>
+
+      <input type="button" value="Login" />
+    </form>
+  </body>
+</html>
---
+++ soruce/utils/header.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="./soruce/style/header.css" />
+<header>
+    <div class="judul">
+        <img src="./soruce/asset/logo_utm.png" alt="logo himapif" />
+        <p>Pendidikan Informatika | Fakultas Ilmu Pendidikan</p>
+    </div>
+    <div class="profile">
+        <p>Profile Admin</p>
+        <a href=""><img src="" alt="" /> <img src="../asset/user-icon.png" alt="" /></a>
+    </div>
+</header>
